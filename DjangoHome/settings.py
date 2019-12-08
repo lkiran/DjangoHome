06f4a1fb6ff454c85aa6978463cd7e36b9e0026a
Levent Kıran <leventkiran@hotmail.com.tr>
2019-12-08 22:56:02 +0300

    🎉 Python version is upgraded ✨
    + Imports are fixed.
    + unicodes are removed.
    + MIDDLEWARE_CLASSES renamed as MIDDLEWARE in settings.py.
    + requirements.txt is updated.

--- DjangoHome/settings.py
+++ DjangoHome/settings.py
@@ -39,17 +39,16 @@ INSTALLED_APPS = ['app',
                   'corsheaders',
                   ]
 
-MIDDLEWARE_CLASSES = ['django.middleware.security.SecurityMiddleware',
-                      'django.contrib.sessions.middleware.SessionMiddleware',
-                      'django.middleware.common.CommonMiddleware',
-                      'django.middleware.csrf.CsrfViewMiddleware',
-                      'django.contrib.auth.middleware.AuthenticationMiddleware',
-                      'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
-                      'django.contrib.messages.middleware.MessageMiddleware',
-                      'django.middleware.clickjacking.XFrameOptionsMiddleware',
-                      'corsheaders.middleware.CorsMiddleware',
-                      'django.middleware.common.CommonMiddleware',
-                      ]
+MIDDLEWARE = ['django.middleware.security.SecurityMiddleware',
+              'django.contrib.sessions.middleware.SessionMiddleware',
+              'django.middleware.common.CommonMiddleware',
+              'django.middleware.csrf.CsrfViewMiddleware',
+              'django.contrib.auth.middleware.AuthenticationMiddleware',
+              'django.contrib.messages.middleware.MessageMiddleware',
+              'django.middleware.clickjacking.XFrameOptionsMiddleware',
+              'corsheaders.middleware.CorsMiddleware',
+              'django.middleware.common.CommonMiddleware',
+              ]
 
 CORS_ORIGIN_ALLOW_ALL = True
 
