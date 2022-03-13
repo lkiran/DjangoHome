8f107cd37f20301447bb9f47f579f5b1ef830cef
LEVENT KIRAN <levent.kiran@adesso.com.tr>
2022-03-13 15:50:28 +0300

    add ENABLE_FILE_LOGGING env variable

--- manage.py
+++ manage.py
@@ -22,27 +22,27 @@ if __name__ == "__main__":
 	#   D - Days
 	#   midnight - roll over at midnight
 	#   W{0-6} - roll over on a certain day; 0 - Monday
-	log_file_name = 'Logs/log'
 	logging_level = logging.DEBUG
 	formatter = logging.Formatter(
 		fmt='%(asctime)s,%(msecs)d - %(name)s - %(levelname)s - %(message)s',
 		datefmt='%b %d, %a %H:%M:%S'
 	)
-
-	if not os.path.exists("./Logs"):
-		os.makedirs("./Logs", 777)
-
 	logger = logging.getLogger()
 	logger.setLevel(logging_level)
-
-	fileHandler = logging.handlers.TimedRotatingFileHandler(filename=log_file_name, when="midnight", interval=1, encoding='utf-8')
-	fileHandler.setFormatter(formatter)
-	fileHandler.suffix = "%d.%B.%Y.log"
-	logger.addHandler(fileHandler)
 
 	consoleHandler = logging.StreamHandler()
 	consoleHandler.setFormatter(formatter)
 	logger.addHandler(consoleHandler)
 
+	if bool(os.environ.get("ENABLE_FILE_LOGGING", 0)):
+		log_file_name = 'Logs/log'
+
+		if not os.path.exists("./Logs"):
+			os.makedirs("./Logs", 777)
+
+		fileHandler = logging.handlers.TimedRotatingFileHandler(filename=log_file_name, when="midnight", interval=1, encoding='utf-8')
+		fileHandler.setFormatter(formatter)
+		fileHandler.suffix = "%d.%B.%Y.log"
+		logger.addHandler(fileHandler)
 
 	execute_from_command_line(sys.argv)
