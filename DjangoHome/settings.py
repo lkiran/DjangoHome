b14288780c4e51190705cd6773b6767327a71b41
LEVENT KIRAN <levent.kiran@adesso.com.tr>
2022-05-02 19:45:23 +0300

    prod deployment

--- DjangoHome/settings.py
+++ DjangoHome/settings.py
@@ -15,7 +15,7 @@ import posixpath
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+print("BASE_DIR: " + BASE_DIR)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
@@ -73,7 +73,7 @@ WSGI_APPLICATION = 'DjangoHome.wsgi.application'
 DATABASES = {
 	'default': {
 		'ENGINE': 'django.db.backends.sqlite3',
-		'NAME': os.path.join(BASE_DIR, 'HomeAutomation.sqlite3'),
+		'NAME': os.path.join(BASE_DIR, 'data/HomeAutomation.sqlite3'),
 		# 'USER': 'admin',
 		# 'PASSWORD': '123456',
 		# 'HOST': '127.0.0.1',
@@ -112,4 +112,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_URL = '/static/'
 
-STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['static']))
+STATIC_ROOT = BASE_DIR + "/static"
---
+++ prod.Dockerfile
@@ -0,0 +1,24 @@
+# Use an official Python runtime as a parent image
+FROM python:3.8
+LABEL Home Automation Server
+
+# The enviroment variable ensures that the python output is set straight
+# to the terminal with out buffering it first
+ENV PYTHONUNBUFFERED 1
+ENV PYTHONDONTWRITEBYTECODE 1
+
+# create the appropriate directories
+ENV HOME=/
+ENV APP_HOME=/DjangoHome
+RUN mkdir $APP_HOME
+RUN mkdir $APP_HOME/static
+# Set the working directory to /DjangoHome
+WORKDIR $APP_HOME
+
+# Copy the current directory contents into the container at /DjangoHome
+ADD . $APP_HOME
+
+RUN pip install --upgrade pip && \
+    pip install -r requirements.txt
+
+RUN python manage.py collectstatic --no-input
